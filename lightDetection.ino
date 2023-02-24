6020ebc608895d310d9a0e8514225be4a6961d1a
username_name_hidden <angst.lukas@gmail.com>
2023-02-24 21:59:37 +0100

    changed the code to let an led on the arduino blink

--- lightDetection.ino
+++ lightDetection.ino
@@ -1,4 +1,4 @@
-  /*
+/*
   Blink
 
   Turns an LED on for one second, then off for one second, repeatedly.
@@ -23,14 +23,15 @@
 */
 
 // the setup function runs once when you press reset or power the board
-void setup()
-{
+void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
-void loop()
-{
-  Serial.println("hello");                    // wait for a second
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
 }
