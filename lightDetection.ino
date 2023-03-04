7e60828e2be144661bdf1f0abe64a049a5bf9e2b
username_name_hidden <angst.lukas@gmail.com>
2023-03-04 20:02:20 +0100

    Added function to read an AI brightness sens

--- lightDetection.ino
+++ lightDetection.ino
@@ -26,10 +26,14 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  // initialize serial connection for debugging
+  Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  int analogValue = analogRead(A0);
+  Serial.println(analogValue);
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
